2cd56c62e9cce032dd79f2aecb9c315e350b2892
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-04-13 13:59:56 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,5 +5,11 @@ describe Post do
     post = Post.create!(title: "My title", description: "The post description")
     expect(post).to be_valid
   end
+  
+  it 'has a summary' do
+  post = Post.create!(title: "My title", description: "The post description")
+  expect(post.post_summary).to eq("My title - The post description")
+end
+
 end
 
